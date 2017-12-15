16104e62963c03e102cbd579cf1ed07f867cf362
gskleres <george.skleres@gmail.com>
2017-12-15 02:26:52 -0500

    Data file for installed mod
    Now if the mod is currently installed, the Install button disappears

--- MaloWLauncher/ModModel.cs
+++ MaloWLauncher/ModModel.cs
@@ -80,7 +80,7 @@ namespace MaloWLauncher
                     {
                         this.InstalledText = "(INSTALLED)";
                     }
-                    NotifyPropertyChanged("InstalledText");
+                    NotifyPropertyChanged("CanInstall");
                 }
             }
         }
